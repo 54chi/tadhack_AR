f853597b7ad8d1a1e29b009d3d079d7a38de1dda
54chi <zerocode001@gmail.com>
2016-10-16 09:53:12 -0500

    initial commit

---
+++ Assets/PanelButtons.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelButtons : MonoBehaviour {
+
+    //refrence for the pause menu panel in the hierarchy
+    public GameObject menuPanel;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+    }
+
+    //function to pause the game
+    public void OpenRecipes()
+    {
+        menuPanel.SetActive(true);
+    }
+    //function to unpause the game
+    public void CloseRecipes()
+    {
+        menuPanel.SetActive(false);
+    }
+}
---
+++ Assets/PopupInfoScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using Vuforia;
+using System.Collections;
+
+public class PopupInfoScript : MonoBehaviour, ITrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+
+    private bool mShowGUIButton = false;
+    private Rect mButtonRect = new Rect(50, 50, 120, 60);
+    
+    public GameObject gameUI;  //The actual Game UI
+
+    // Use this for initialization
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(
+                                TrackableBehaviour.Status previousStatus,
+                                TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED)
+        {
+            mShowGUIButton = true;
+        }
+        else
+        {
+            mShowGUIButton = false;
+        }
+        gameUI.SetActive(mShowGUIButton); //hide or show the info panel
+    }
+
+    void OnGUI()
+    {
+        if (mShowGUIButton)
+        {      
+            // draw the GUI button
+//            if (GUI.Button(mButtonRect, "Hello"))
+//            {
+                // do something on button click 
+//            }
+        }
+    }
+}
